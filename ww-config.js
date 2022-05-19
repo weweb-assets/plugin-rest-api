bef6623126c5d81ca2eab9323e20da30e8e94744
Dams-d <damien.duro8@gmail.com>
2022-05-19 17:27:07 +0200

    Change action name

--- ww-config.js
+++ ww-config.js
@@ -10,7 +10,7 @@ export default {
     },
     actions: [
         {
-            name: 'Api Request',
+            name: 'REST API Request',
             code: 'apiRequest',
             isAsync: true,
             /* wwEditor:start */
