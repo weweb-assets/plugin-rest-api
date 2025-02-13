d8175e0496c7f28338b46fb681f0d5e137767942
Marc Fabre <nds.kram@gmail.com>
2025-02-13 09:15:21 +0100

    Feat add copilot config (#30)
    * feat: add action description
    
    * chore: update copilot config
    
    * fix: data type not bindable
    
    * fix: ai description for the return
    
    * fix: response format
    
    * fix: ai description for the return
    
    ---------
    
    Co-authored-by: Alexis JC <alexisjc.dev@gmail.com>

--- ww-config.js
+++ ww-config.js
@@ -21,6 +21,61 @@ export default {
             getIsValid({ url, method }) {
                 return !!url && !!method;
             },
+            copilot: {
+                description: 'Make a REST API request using axios with configurable method, URL, headers, and data',
+                returns: 'any - The result of response.data from the request',
+                schema: {
+                    url: {
+                        type: 'string',
+                        description: 'The URL to request',
+                        bindable: true,
+                    },
+                    method: {
+                        type: 'string',
+                        description: 'The HTTP method to use',
+                        bindable: false,
+                    },
+                    data: {
+                        type: 'Array<{key: string, value: string} or any if raw body is used',
+                        description:
+                            'The data to send, the array will be converted to an object, both key and value can be bound to a formula. You can use a different format by using the raw body option, you can then bind the whole property.',
+                        bindable: true,
+                    },
+                    headers: {
+                        type: 'Array<{key: string, value: string}',
+                        description: 'The headers to send',
+                        bindable: true,
+                    },
+                    queries: {
+                        type: 'Array<{key: string, value: string}',
+                        description: 'The query string parameters',
+                        bindable: true,
+                    },
+                    dataType: {
+                        type: 'string',
+                        description: 'The content type for the request',
+                        bindable: false,
+                    },
+                    isThroughServer: {
+                        type: 'boolean',
+                        description:
+                            'Whether to send the request through the WeWeb server to avoid CORS issues, use with caution, only when you know the request will fail due to CORS. This option will change the returns format to the whole response object instead (status,statusText,data,headers,config,request)',
+                        bindable: true,
+                    },
+                    useRawBody: {
+                        type: 'boolean',
+                        description:
+                            "Whether to send the data without any transformation, required if data doesn't fit the key-value array format",
+                        bindable: true,
+                    },
+                    isWithCredentials: {
+                        type: 'boolean',
+                        description:
+                            'Whether to send credentials with the request, such as cookies, cannot be used with isThroughServer, use with caution, many API will fail if you enable this',
+                        bindable: true,
+                    },
+                },
+            },
             /* wwEditor:end */
         },
     ],
