90462d25ca45360ea6a12948c39b69cbe10e812d
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2025-07-29 15:17:21 +0200

    feat: enhance wwVariableOptions to include element names
    - Updated the wwVariableOptions computed property to append element names to variable labels when applicable.
    - Introduced a new helper function `getElementName` to retrieve the name of elements based on their UID.

--- src/components/ApiRequest.vue
+++ src/components/ApiRequest.vue
@@ -182,6 +182,11 @@
 </template>
 
 <script>
+const getElementName = uid => {
+    const { name } = wwLib.$store.getters['websiteData/getWwObjects'][uid] || { name: 'Element undefined' };
+    return name;
+};
+
 export default {
     props: {
         plugin: { type: Object, required: true },
@@ -279,11 +284,21 @@ export default {
         wwVariableOptions() {
             return this.wwVariables
                 .filter(variable => variable.type === 'array')
-                .map(variable => ({
-                    label: variable.name,
-                    value: variable.id,
-                    icon: variable.type,
-                }));
+                .map(variable => {
+                    let label = variable.name;
+                    const elementName =
+                        variable.componentType && variable.componentType === 'element'
+                            ? getElementName(variable.componentUid)
+                            : null;
+
+                    if (elementName) label = `${elementName} - ${variable.name}`;
+
+                    return {
+                        label,
+                        value: variable.id,
+                        icon: variable.type,
+                    };
+                });
         },
     },
     mounted() {
